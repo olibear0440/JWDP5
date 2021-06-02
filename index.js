b0315ad2b166ea11b099e3ff79b63da6afa9faa7
olivierbenoit <olivier.bent@gmail.com>
2021-06-02 14:45:41 +0200

    rajout Attribut dans fonction listProduct

--- index.js
+++ index.js
@@ -1,18 +1,4 @@
-/*class teddyBear {
-    constructor(objectId, name, price, description, imageUrl){
-        this.objectId = objectId;
-        this.name = name;
-        this.price = price;
-        this.description = description;
-        this.imageUrl = imageUrl;
-    }
-}
-let teddyOne = new teddyBear("5be9c8541c9d440000665243", "Norbert", 2900, "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", "http://localhost:3000/images/teddy_1.jpg");
-let teddyTwo = new teddyBear("5beaa8bf1c9d440000a57d94", "Arnold", 3900, "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", "http://localhost:3000/images/teddy_2.jpg");
-let teddyThree = new teddyBear("5beaaa8f1c9d440000a57d95", "Lenny and Carl", 5900, "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", "http://localhost:3000/images/teddy_3.jpg");
-let teddyFour = new teddyBear("5beaabe91c9d440000a57d96", "Gustav", 4500, "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", "http://localhost:3000/images/teddy_4.jpg");
-let teddyFive = new teddyBear("5beaacd41c9d440000a57d97", "Garfunkel", 5500, "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", "http://localhost:3000/images/teddy_5.jpg");
-*/
+
 const product = document.getElementById("product")
 const btn = document.getElementById("btn")
 
@@ -23,16 +9,19 @@ function listProduct(){
     .then((data) =>{
         for(let i=0; i< data.length; i++){
             let div = document.createElement("div");
+            div.setAttribute("class", "productRef");
     
             let img = document.createElement("img");
             img.src= data[i].imageUrl;
             div.appendChild(img);
     
             let spanName = document.createElement("div");
+            spanName.setAttribute("class", "productName");
             spanName.innerText = "Name: " + data[i].name;
             div.appendChild(spanName);
     
             let price = document.createElement("div");
+            price.setAttribute("class", "productPrice")
             price.innerText = "Price: " + data[i].price + "€";
             div.appendChild(price);
     
@@ -40,19 +29,13 @@ function listProduct(){
         }
     })
 }
-// creation fonction toggle sur le btn Oritoy
-function btnToggle() {
-    if (product.style.display === "none") {
-      product.style.display = "block";
-    } else {
-      product.style.display = "none";
-    }
-}
-// evenement au click du btn
-btn.addEventListener("click", function(){
-    listProduct()
-    btnToggle()
-})
+listProduct()
+
+//style de l'id product
+product.className = "styleProduct";
+
+let productName = document.getElementsByClassName("productName")
+
 
 
 
